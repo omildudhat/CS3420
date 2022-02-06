4a558ed136f81a5b09108813fbfc3c6b652733e3
ynova019 <98299264+ynova019@users.noreply.github.com>
2022-02-05 20:57:36 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Function1
+//
+//  Created by Omil Dudhat on 2/4/22.
+//
+
+#include <iostream>
+using namespace std;
+
+void (int a=10, int b=20, int c=30)
+{
+    if(a)
+    
+}
